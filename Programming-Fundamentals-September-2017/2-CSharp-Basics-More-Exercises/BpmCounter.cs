c3cd745cc71443f23750efb2e852deb8d260d02e
Zhelyan Radoev <jelioradoev@gmail.com>
2017-09-27 10:09:22 +0300

    Added solutions for homework exercises and labs

---
+++ Programming-Fundamentals-September-2017/2-CSharp-Basics-More-Exercises/BpmCounter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BPM_Counter
+{
+    class BpmCounter
+    {
+        static void Main(string[] args)
+        {
+            int beatsPerMinute = int.Parse(Console.ReadLine());
+            int NumberOfBeats = int.Parse(Console.ReadLine());
+
+            double bars = (double)NumberOfBeats / 4;
+            double sequenceSeconds = Math.Floor(60 / (double)beatsPerMinute * NumberOfBeats);
+            int sequenceMinutes = 0;
+
+            while (sequenceSeconds >= 60)
+            {
+                sequenceMinutes++;
+                sequenceSeconds -= 60;
+            }
+
+            Console.WriteLine($"{Math.Round(bars, 1)} bars - {sequenceMinutes}m {sequenceSeconds}s");
+        }
+    }
+}
---
+++ Programming-Fundamentals-September-2017/2-CSharp-Basics-More-Exercises/Megapixels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Megapixels
+{
+    class Megapixels
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double megaPixels = (width * height) / 1000000;
+
+            Console.WriteLine($"{width}x{height} => {Math.Round(megaPixels, 1)}MP");
+        }
+    }
+}
