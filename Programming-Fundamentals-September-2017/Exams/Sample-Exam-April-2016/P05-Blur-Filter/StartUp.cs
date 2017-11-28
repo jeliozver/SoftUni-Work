d5e3e2257c2a33bd81e4e7a2e653aa981f99b8e7
Zhelyan Radoev <jelioradoev@gmail.com>
2017-11-28 16:21:12 +0200

    added solution

--- Programming-Fundamentals-September-2017/Exams/Sample-Exam-April-2016/P05-Blur-Filter/StartUp.cs
+++ Programming-Fundamentals-September-2017/Exams/Sample-Exam-April-2016/P05-Blur-Filter/StartUp.cs
@@ -1,12 +1,73 @@
-﻿namespace P05_Blur_Filter
+namespace P05_Blur_Filter
 {
     using System;
+    using System.Linq;
 
     public class StartUp
     {
         public static void Main()
         {
-            // TO DO...
+            int blurAmount = int.Parse(Console.ReadLine());
+
+            int[] dimensions = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            long[,] matrix = new long[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = int.Parse(line[col]);
+                }
+            }
+
+            int[] blurCoordinates = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int blurRow = blurCoordinates[0];
+            int blurCol = blurCoordinates[1];
+
+            int startRow = blurRow - 1;
+            int endRow = blurRow + 1;
+            int startCol = blurCol - 1;
+            int endCol = blurCol + 1;
+
+            for (int row = startRow; row <= endRow; row++)
+            {
+                for (int col = startCol; col <= endCol; col++)
+                {
+                    try
+                    {
+                        matrix[row, col] += blurAmount;
+                    }
+                    catch (Exception)
+                    {
+                       
+                    }
+                }
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
